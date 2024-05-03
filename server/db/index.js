9757e5c4994062d2abb1547615115b02fc08c478
Mubashir Tanwar <tanwar0210@gmail.com>
2024-05-04 01:02:12 +0530

    add README.md

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
-        const connection = await mongoose.connect('mongodb+srv://tanwar0210:MaWX3WMuY5PidZJe@nuitritionist.dbuw3gf.mongodb.net/?retryWrites=true&w=majority&appName=Nuitritionist');
+        const connection = await mongoose.connect(process.env.MONGO_URI);
         console.log(`\n MONGPDB connected!! HOST: ${connection.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection error", error);
